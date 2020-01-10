e0930d9e28708d84408842ffc047459d39103de6
sbatsch <sbatsch2@gmail.com>
2020-01-10 10:37:21 -0600

    new commit

--- chicago_employees.rb
+++ chicago_employees.rb
@@ -6,17 +6,20 @@ employees = response.parse
 employees.each do |employee|
   puts "Name: #{employee["name"]}"
   puts "Job: #{employee["job_titles"]}"
-  puts "Department: #{employee["department"]}"
+  puts "Dept: #{employee["department"]}"
 
-  # salary_or_hourly 
+  puts
 
-if employee["salary_or_hourly"] == "salary"
-   puts "salary: #{employee["annual_salary"]}"
-else 
-  puts "Typical Hours: #{employee["typical_hours"]}"
-  puts "Hourly Rate: #{employee["hourly_rate"]}"
-  puts "Estimated Salary: #{employee["estimated_salary"]}"
-end 
-  puts "=" * 30 
-end 
+  if employee["salary_or_hourly"] == "Salary"
+    puts "Salary: $#{employee["annual_salary"]}"
+  else
+    puts "Typical Hours: #{employee["typical_hours"]}"
+    puts "Hourly Rate: #{employee["hourly_rate"]}"
+    rate_per_week = employee["typical_hours"].to_i * employee["hourly_rate"].to_f
+    puts "Estimated Salary: $#{ rate_per_week * 50 }"
+  end
 
+  puts
+  puts "=" * 30
+  puts
+end
